ad46e285e2c0ca4e0215dea2b0fc4ba1ebe32a03
Kibreab Aklilu <Akkeba37@gmail.com>
2025-03-19 16:50:34 +0300

    Add Card component and update Home page layout with project, task, user, and client statistics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,37 @@
+import Card from "@/components/ui/Card";
+import { Briefcase, ListChecks, Users, UserCheck } from "lucide-react";
+
 export default function Home() {
   return (
-    <div>
-      <h2>Hi</h2>
+    <div className="flex flex-wrap -mx-2 mt-4">
+      <Card
+        title="Total Projects"
+        count={6}
+        link="/projects"
+        Icon={Briefcase}
+        color="green-500"
+      />
+      <Card
+        title="Total Tasks"
+        count={45}
+        link="/tasks"
+        Icon={ListChecks}
+        color="blue-500"
+      />
+      <Card
+        title="Total Users"
+        count={12}
+        link="/users"
+        Icon={Users}
+        color="yellow-500"
+      />
+      <Card
+        title="Total Clients"
+        count={1}
+        link="/clients"
+        Icon={UserCheck}
+        color="cyan-500"
+      />
     </div>
   );
 }
